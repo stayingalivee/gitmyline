7fc2691bde7aece4fe27d455fe115bcd94d050ac
Ahmadnoid <ahmadnoid@Ahmadnoids-MacBook-Air.local>
2023-11-05 15:15:29 +0300

    feat: add gitlab support

--- src/parser.ts
+++ src/parser.ts
@@ -2,22 +2,24 @@ import { stringify } from 'querystring';
 import * as vscode from 'vscode';
 
 const { exec } = require('child_process');
-const orange = vscode.window.createOutputChannel("Orange");
+const debugChannel = vscode.window.createOutputChannel("gitmylineChannel");
 
 export function getCursorUpstreamPath() {
     const folder = <string> vscode.workspace.workspaceFolders?.[0].uri.fsPath.replace("file://", "")
     const file =  (<string> vscode.window.activeTextEditor?.document.fileName)?.substring(folder.length)
     const line = (<number> vscode.window.activeTextEditor?.selection.active.line) + 1
 
+    debugChannel.appendLine(folder)
+    debugChannel.appendLine(file)
+    debugChannel.appendLine(String(line))
+
     const command = ["(cd", folder, ";", "git remote -v)"].join(" ")
     exec(command, (err: any, stdout: any, stderr: any) => {
-        
-        const upstreamUrl = parse(stdout.split("\t")[1].split(" ")[0])
-        const upstreamPath = getFullPath(upstreamUrl, file, line)
+        const upstreamProjectUrl = parse(stdout.split("\t")[1].split(" ")[0])
+        const upstreamCursorPath = getFullPath(upstreamProjectUrl, file, line)
 
-        openInBrowser(upstreamPath)
+        openInBrowser(upstreamCursorPath)
     });
-          
 }
 
 function parse(url: string) {
@@ -25,8 +27,8 @@ function parse(url: string) {
 }
 
 function getFullPath(upstreamUrl: string, file: string, line: number) {
-    // TODO: add gitlab support
-    return upstreamUrl += "/blob/main" + file + "#L" + line;
+    const blobPrefix = upstreamUrl.includes("gitlab") ? "/-" : ""
+    return upstreamUrl += blobPrefix+ "/blob/main" + file + "#L" + line;
 }
 
 function openInBrowser(url: string) {
